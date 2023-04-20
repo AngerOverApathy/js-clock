ad5bbae2abd0da7d305dc2490d79e2ddadc4f91d
AngerOverApathy <violasian@gmail.com>
2023-04-19 17:51:37 -0700

    styling

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 function setDate(){
-    let secondHand = document.querySelector('.second-hand')
-
     const now = new Date()
+    const hour = now.getHours()
+    const minute = now.getMinutes()
     const seconds = now.getSeconds()
-    const secondsDegrees = (seconds / 60) * 360
 
+    let secondHand = document.querySelector('.second-hand')
+    const secondsDegrees = ((seconds / 60) * 360) + 90 //90 added to offset 90deg styling
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-    console.log(seconds)
+
+    let minuteHand = document.querySelector('.min-hand')
+    const minuteDegrees = ((minute / 60) * 360) + 90
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`
+
+    let hourHand = document.querySelector('.hour-hand')
+    const hoursDegrees = ((hour / 60) * 360) + 90 
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
 }
 
 setInterval(setDate, 1000)
